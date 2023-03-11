364c36918a7ac6d68a8388fd19437bedff1144b7
ZiqingZhou694 <113397683+ZiqingZhou694@users.noreply.github.com>
2023-03-11 01:08:47 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -48,10 +48,10 @@ function draw() {
 
 function mousePressed() {
   for (let i = bugs.length - 1; i >= 0; i--) {
-    if (bugs[i].isClicked(mouseX, mouseY)) {   
-        bugs[i].dead = true;
-        squishedCount++; // increment squished count
-        break;  
+    if (bugs[i].isClicked(mouseX, mouseY)) {
+      bugs[i].dead = true;
+      squishedCount++; // increment squished count
+      break;
     }
   }
 }
